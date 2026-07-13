171a3711689b01d3dfb13de516b79abaa22e706f
Marjolijn-v <mverspiek@gmail.com>
2026-07-13 14:33:51 +0200

    First commit

---
+++ src/main/java/nl/novi/eindopdrachtbackendlibrary/EindopdrachtBackendLibraryApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackendlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtBackendLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/eindopdrachtbackendlibrary/EindopdrachtBackendLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackendlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
