c1c0f03c9ded0051ac4867eaa82aabe941ec22da
Marjolijn-v <mverspiek@gmail.com>
2026-07-13 16:24:17 +0200

    AuthorEntity invulling gegeven, gestart met BookEntity

--- src/main/java/nl/novi/eindopdrachtbackendlibrary/entities/AuthorEntity.java
+++ src/main/java/nl/novi/eindopdrachtbackendlibrary/entities/AuthorEntity.java
@@ -1,4 +1,42 @@
 package nl.novi.eindopdrachtbackendlibrary.entities;
 
-public class AuthorEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+public class AuthorEntity extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    @ManyToMany(mappedBy = "authors")
+    private Set<BookEntity> books = new HashSet<>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<BookEntity> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<BookEntity> books) {
+        this.books = books;
+    }
 }
--- src/main/java/nl/novi/eindopdrachtbackendlibrary/entities/BookEntity.java
+++ src/main/java/nl/novi/eindopdrachtbackendlibrary/entities/BookEntity.java
@@ -1,4 +1,19 @@
 package nl.novi.eindopdrachtbackendlibrary.entities;
 
-public class BookEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class BookEntity extends BaseEntity{
+    private String title;
+    private int releaseYear;
+    private AuthorEntity author;
+    private String description;
+
+//    picture en numberOfCopies toevoegen.
+
+
+
+
 }
