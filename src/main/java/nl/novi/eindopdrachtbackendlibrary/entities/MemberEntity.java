6ef0266228d057aa9d3339f57fb807b7e8a1e650
Marjolijn-v <mverspiek@gmail.com>
2026-07-13 16:42:35 +0200

    MemberEntity invulling gegeven

--- src/main/java/nl/novi/eindopdrachtbackendlibrary/entities/MemberEntity.java
+++ src/main/java/nl/novi/eindopdrachtbackendlibrary/entities/MemberEntity.java
@@ -1,4 +1,67 @@
 package nl.novi.eindopdrachtbackendlibrary.entities;
 
-public class MemberEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "members")
+public class MemberEntity extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime dob;
+    private List<LoanActivityEntity> loans = new ArrayList<>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
+
+    public List<LoanActivityEntity> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(List<LoanActivityEntity> loans) {
+        this.loans = loans;
+    }
 }
