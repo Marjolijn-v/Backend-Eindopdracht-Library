86042f6390fb61762f29bf8524f38e42d42633ac
Marjolijn-v <mverspiek@gmail.com>
2026-07-16 11:09:47 +0200

    LoanActivity Dto's

--- src/main/java/nl/novi/eindopdrachtbackendlibrary/dtos/loanActivity/LoanActivityRequestDto.java
+++ src/main/java/nl/novi/eindopdrachtbackendlibrary/dtos/loanActivity/LoanActivityRequestDto.java
@@ -1,4 +1,46 @@
 package nl.novi.eindopdrachtbackendlibrary.dtos.loanActivity;
 
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
 public class LoanActivityRequestDto {
+    @NotNull
+    private Long bookId;
+    @NotNull
+    private Long userId;
+    private LocalDateTime loanDate;
+    private LocalDateTime returnDate;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDateTime loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
 }
--- src/main/java/nl/novi/eindopdrachtbackendlibrary/dtos/loanActivity/LoanActivityResponseDto.java
+++ src/main/java/nl/novi/eindopdrachtbackendlibrary/dtos/loanActivity/LoanActivityResponseDto.java
@@ -1,4 +1,54 @@
 package nl.novi.eindopdrachtbackendlibrary.dtos.loanActivity;
 
+import nl.novi.eindopdrachtbackendlibrary.dtos.book.BookResponseDto;
+import nl.novi.eindopdrachtbackendlibrary.dtos.user.UserResponseDto;
+
+import java.time.LocalDateTime;
+
 public class LoanActivityResponseDto {
+    private Long id;
+    private BookResponseDto book;
+    private UserResponseDto user;
+    private LocalDateTime loanDate;
+    private LocalDateTime returnDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BookResponseDto getBook() {
+        return book;
+    }
+
+    public void setBook(BookResponseDto book) {
+        this.book = book;
+    }
+
+    public UserResponseDto getUser() {
+        return user;
+    }
+
+    public void setUser(UserResponseDto user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDateTime loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
 }
